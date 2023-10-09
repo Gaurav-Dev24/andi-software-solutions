a197ebb5da079b011a8c4002a59c8f294c5ead2b
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-10-09 12:32:20 +0530

    Navbar created and marquee tag added to the header

--- andi-website/src/components/Header/Header.js
+++ andi-website/src/components/Header/Header.js
@@ -3,14 +3,21 @@ import "./Header.css";
 import Model from "../Model/Model";
 import Navbar from "../Navbar/Navbar";
 
-
 const Header = () => {
-
-
   return (
     <header className="main-header">
       <Model />
       <Navbar />
+      <div className="sliding-text">
+        <marquee behavior="scroll" direction="left">
+          <p>
+            <a href="https://andisoftwaresolutions.com/index.html"> </a>
+            <a href="https://andisoftwaresolutions.com/franchise.php">
+              Freelancers/Franchise/Reseller Required in all major cities
+            </a>
+          </p>
+        </marquee>
+      </div>
     </header>
   );
 };
